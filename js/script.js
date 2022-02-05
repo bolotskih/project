7680c153d94e189ed17d633adb7b1a39b96a6936
Anton <gafer.bae@icloud.com>
2022-02-05 09:42:45 +0300

    прописал юзстрикт

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-alert(1);
+"use strict";
+let numberOfFilms = +prompt("Сколько фильмов вы уже смотрели", "1");
+
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies:{},
+    actors: {},
+    genres: [],
+    privat: false
+};
+console.log(personalMovieDB.count);
+
+let a = prompt("Один из последних фильмов", "");
+let b = +prompt("на сколько оцените его","1");
+
+personalMovieDB.movies[a] = b;
+console.log(personalMovieDB.movies);
